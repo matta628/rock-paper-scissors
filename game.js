61c2efa92b112f28c1d486e6028ee5dd4e134373
Matt <matta628@gmail.com>
2022-11-10 00:20:02 -0500

    Announce winner of overall game

--- game.js
+++ game.js
@@ -1,5 +1,4 @@
 function getComputerChoice(){
-    //Should randomly return either rock paper or scissors
     let num = Math.floor((Math.random() * 3));
     if (num == 0){
         return "Rock";
@@ -10,54 +9,43 @@ function getComputerChoice(){
     return "Scissors";
 }
 
-function playRound(playerChoice, computerChoice){
-    playerChoice = playerChoice.slice(0,1).toUpperCase() + playerChoice.slice(1).toLowerCase();
-    if (playerChoice == computerChoice){
-        return "It's a tie!";
-    }
-    else if (playerChoice == "Rock" && computerChoice == "Scissors" ||
-        playerChoice == "Scissors" && computerChoice == "Paper" ||
-        playerChoice == "Paper" && computerChoice == "Rock"){
-        return `You won! ${playerChoice} beats ${computerChoice}`;
-    }
-    return `Computer wins! ${computerChoice} beats ${playerChoice}`;
-}
-
 let playerScore = 0;
 let computerScore = 0;
+
+function playRound(){
+    if (playerScore >= 5 || computerScore >= 5){
+        return;
+    }
+
+    let playerChoice = this.textContent;
+    playerChoice = playerChoice.slice(0,1).toUpperCase() + playerChoice.slice(1).toLowerCase();
+    let computerChoice = getComputerChoice();
+
+    const resultsDiv = document.querySelector('.results');
+    if (playerChoice == computerChoice){
+        resultsDiv.textContent = "It's a tie!";
+    }
+    else if (playerChoice == "Rock" && computerChoice == "Scissors" ||
+            playerChoice == "Scissors" && computerChoice == "Paper" ||
+            playerChoice == "Paper" && computerChoice == "Rock"){
+        playerScore++;
+        const playerDiv = document.querySelector('.player');
+        playerDiv.textContent = `Player: ${playerScore}`;
+        resultsDiv.textContent = playerScore == 5 ? 
+        "YOU WON THE GAME YASSSSSS" : `Big L! ${playerChoice} beats ${computerChoice}`;
+    }else{
+        computerScore++;
+        const computerDiv = document.querySelector('.computer');
+        computerDiv.textContent = `Computer: ${computerScore}`;
+        resultsDiv.textContent = computerScore == 5 ?
+        "THE HUNK OF METAL BEAT YOU..." : `Computer W! ${computerChoice} beats ${playerChoice}`;
+    }
+
+    
+}
 
 const buttons = document.querySelectorAll('button');
 console.log(buttons);
 buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        let roundResult = playRound(button.textContent,getComputerChoice());
-        const divResults = document.querySelector('.results');
-        divResults.textContent = roundResult;
-        if (roundResult.charAt(0) == "Y") playerScore++;
-        if (roundResult.charAt(0) == "C") computerScore++;
-        const playerDiv = document.querySelector('.player');
-        playerDiv.textContent = `Player: ${playerScore}`;
-        const computerDiv = document.querySelector('.computer');
-        computerDiv.textContent = `Computer: ${computerScore}`;
-    });
+   button.addEventListener('click', playRound);
 });
-
-function game(){
-    for (let i = 0; i < 5; i++){
-        playerChoice = prompt("Choose rock paper or scissors!","").trim();
-        let result = playRound(playerChoice, getComputerChoice());
-        console.log(result);
-        if (result.charAt(0) == "Y") playerScore++;
-        if (result.charAt(0) == "C") computerScore++;
-        console.log(`\tPlayer: ${playerScore}\tComputer: ${computerScore}`);
-    }
-    if (playerScore == computerScore){
-        console.log("No one won this game. But no won lost either!")
-    }
-    else if (playerScore > computerScore){
-        console.log("You beat the computer yasssss")
-    }
-    else{
-        console.log("Nice one. Got beat by a hunk of metal...")
-    }
-}
